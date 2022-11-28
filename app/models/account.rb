99ee605212eb8ad8efbbf28097944a2c8ded5087
Ugyen Choden <ugyen.choden@selise.ch>
2022-11-28 13:51:13 +0600

    Go rails

---
+++ app/models/account.rb
@@ -0,0 +1,6 @@
+class Account < ApplicationRecord
+  has_many :addresses
+
+  has_one :shipping_address, -> { where(address_type: :shipping) }, class_name: "Address"
+  has_one :billing_address, -> { where(address_type: :billing) }, class_name: "Address"
+end
